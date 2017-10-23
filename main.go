c74c30a8e5c0e2741722686ea168bd4b8469c65a
Bianca Rosa <me@biancarosa.com.br>
2017-10-23 14:38:14 -0200

    add poc

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	appd "github.com/stone-payments/go-appdynamics"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AppDynamicsMiddleware() func(*gin.Context) {
+
+	cfg := appd.Config{}
+
+	cfg.AppName = "TestAppD"
+	// cfg.TierName = conf.Name
+	// cfg.NodeName = conf.Hostname
+	// cfg.Controller.Host = conf.AppDynamics.Host
+	// cfg.Controller.Port = uint16(conf.AppDynamics.Port)
+	// cfg.Controller.UseSSL = conf.AppDynamics.UseSSL
+	// cfg.Controller.Account = conf.AppDynamics.Account
+	// cfg.Controller.AccessKey = conf.AppDynamics.AccessKey
+	cfg.InitTimeoutMs = 0
+
+	if err := appd.InitSDK(&cfg); err != nil {
+		fmt.Printf("Error initializing the AppDynamics SDK - %#v\n", err.Error())
+	} else {
+		fmt.Printf("Initialized AppDynamics SDK successfully\n")
+	}
+
+	return func(c *gin.Context) {
+		route := c.Request.Method + " " + c.Request.URL.Path
+		for k := range c.Params {
+			p := c.Params[k].Value
+			route = strings.Replace(route, p, "*", -1)
+		}
+
+		// Start a AppDynamics business transaction
+		btHandle := appd.StartBT(route, "")
+
+		// Call pending handlers
+		c.Next()
+
+		fmt.Printf("%#v", c.Writer.Status())
+
+		// End and save the business transaction
+		appd.EndBT(btHandle)
+	}
+
+}
+
+func main() {
+	r := gin.Default()
+	r.Use(AppDynamicsMiddleware())
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.GET("/ping/:me", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
+}
